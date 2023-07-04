c669d31553bb8b43e7b344ba606e8e9665170570
hitprofit70 <hitprofit70@gmail.com>
2023-07-04 10:11:05 +0200

    bootstrap added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div>
+            <nav className="navbar bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand text-white">TIK TOK APP</a>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Header
