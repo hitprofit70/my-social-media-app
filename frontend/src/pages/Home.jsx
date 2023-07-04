cc6f5bb2d875af40272e23bc41f44428c20b13d5
hitprofit70 <hitprofit70@gmail.com>
2023-07-04 10:55:05 +0200

    navbar and leftsidebar added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import LeftSideBar from '../components/LeftSideBar'
 
 const Home = () => {
   return (
     <div>
     <Header/>
+    <LeftSideBar/>
     </div>
   )
 }
